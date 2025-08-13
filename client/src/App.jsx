a80d63debb68a07280485182cf1ca1bdc4771a0e
debalina589 <114078959+debalina589@users.noreply.github.com>
2025-08-13 12:09:03 +0000

    frontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-
+import Navbar from './components/Navbar'
+import { Route, Routes,useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBooking from './pages/MyBooking'
+import Favorite from './pages/Favorite'
+import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer'
 const App = () => {
-    return (
-        <div>
 
-        </div>
+    const isAdminRoute = useLocation().pathname.startswith('/admin')
+    return (
+        <>
+        <Toaster/>
+          {!isAdminRoute && <Navbar/>}
+          <Routes>
+            <Route path='/' element={<Home/>} />
+           <Route path='/movies' element={<Movies/>} />
+             <Route path='/movies/:id' element={<MovieDetails/>} />
+             <Route path='/movies/:id' element={<MovieDetails/>} />
+             <Route path='/movies/:id/date' element={<SeatLayout/>} />
+             <Route path='/my-bookings' element={<MyBooking/>} />
+             <Route path='/favorite' element={<Favorite/>} />
+           {!isAdminRoute && <Footer/>}
+          </Routes>
+        </>
     )
 }
 
