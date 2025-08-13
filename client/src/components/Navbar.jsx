e3cf5a32926f8c20d4e460a3c7f4a2b9ca3c5057
debalina589 <114078959+debalina589@users.noreply.github.com>
2025-08-13 16:34:44 +0000

    fronted setup

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { assets } from '../assets/assets'
+import { MenuIcon, SearchIcon } from 'lucide-react'
 
 const Navbar = () => {
     return(
+    <div className='fixed top-0 left-0 z-50 w-full items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
+    <Link to='/' className='max-md:flex-1'>
+
+    <img src={assets.logo} alt="" className='w-36 h-auto'/>
+    </Link>
     <div>
-      <h1>Navbar</h1>  
+      <XIcon/>
+      <Link to='/'>Home</Link>
+      <Link to='/movies'>Movies</Link>
+      <Link to='/'>Theaters</Link>
+      <Link to='/'>Realeases</Link>
+      <Link to='/favorites'>Favorites</Link>
+    </div>
+      <div className='flex items-center gap-8'>
+        <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
+        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full'>Login</button>
+      </div>
+      <MenuIcon className='max-md:ml-4 md-hidden w-8 h-8 cursor-pointer/>  
     </div>
   )
 
