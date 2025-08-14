3704a2c2d4a3bdd8e2e6b2277cf382e9bd688977
debalina589 <114078959+debalina589@users.noreply.github.com>
2025-08-14 11:20:53 +0000

    frontend setup

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,29 +2,42 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { MenuIcon, SearchIcon } from 'lucide-react'
+import {useClerk, UserButton, useUser} from '@clerk/clerk-react'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = userState(false)
+  const {user} = useUser
+  const {openSignIn} = useClerk()
     return(
-    <div className='fixed top-0 left-0 z-50 w-full items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
+    <div className='fixed top-0 left-0 z-50 w-full items-center justify-between px-6 md:px-16 1g:px-36 py-5' >
     <Link to='/' className='max-md:flex-1'>
 
     <img src={assets.logo} alt="" className='w-36 h-auto'/>
     </Link>
-    <div>
-      <XIcon/>
-      <Link to='/'>Home</Link>
-      <Link to='/movies'>Movies</Link>
-      <Link to='/'>Theaters</Link>
-      <Link to='/'>Realeases</Link>
-      <Link to='/favorites'>Favorites</Link>
+    <div classname='{max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-1g z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:round-full backdrop-blur bg-black/70 md:bg-white/10
+    md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? max-md:w-full:max-md:w-0}}'>
+      <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={()=> setIsOpen(!isOpen)}/>
+      <Link onClick={()=> {scrollTo(0,0),setIsOpen(false)}} to='/'>Home</Link>
+      <Link onClick={()=> {scrollTo(0,0), setIsOpen(false)}} to='/movies'>Movies</Link>
+      <Link onClick={()=> {scrollTo(0,0), setIsOpen(false)}} to='/'>Theaters</Link>
+      <Link onClick={()=> {scrollTo(0,0), setIsOpen(false)}} to='/'>Realeases</Link>
+      <Link onClick={()=> {scrollTo(0,0) ,setIsOpen(false)}} to='/favorite'>Favorites</Link>
     </div>
       <div className='flex items-center gap-8'>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
-        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full'>Login</button>
+        {
+          !user ? (
+              <button onClick={openSignIn}classname='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull 
+              transition rounded-full font-medium cursor-pointer'>Login</button>
+          ): (
+            <UserButton/>
+          )
+        }
+        x
       </div>
-      <MenuIcon className='max-md:ml-4 md-hidden w-8 h-8 cursor-pointer/>  
+      <MenuIcon className='max-md:ml-4 md-hidden w-8 h-8 cursor-pointer'onClick={()=> setIsOpen(!isOpen)}/>  
     </div>
   )
 
   }
-export default Navbar
+ export default Navbar
