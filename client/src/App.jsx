2d49e3b21af6d204b794d5381dbe374ea1f23013
null <null>
2025-08-17 14:43:04 +0530

    git commit -m "frontend setup"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes,useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import MovieDetails from './pages/MovieDetails'
 import SeatLayout from './pages/SeatLayout'
@@ -19,9 +19,8 @@ const App = () => {
             <Route path='/' element={<Home/>} />
            <Route path='/movies' element={<Movies/>} />
              <Route path='/movies/:id' element={<MovieDetails/>} />
-             <Route path='/movies/:id' element={<MovieDetails/>} />
              <Route path='/movies/:id/date' element={<SeatLayout/>} />
-             <Route path='/my-bookings' element={<MyBooking/>} />
+             <Route path='/my-bookings' element={<MyBookings/>} />
              <Route path='/favorite' element={<Favorite/>} />
            {!isAdminRoute && <Footer/>}
           </Routes>
